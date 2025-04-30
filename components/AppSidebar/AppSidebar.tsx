d6cb49be3b88ac448e4746aaaa410e4ccdb4b8bf
Vivek205 <ichbinvivek@gmail.com>
2025-04-30 11:11:20 +0200

    Basic Sidebar

---
+++ components/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,31 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar";
+import Link from "next/link";
+import { menuItems } from "./constants";
+
+export const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>ChromeExt Analytics</SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu>
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link href="/">
+                  <item.icon /> {item.title}
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
---
+++ components/AppSidebar/constants.ts
@@ -0,0 +1,8 @@
+import { BadgeInfo, CircleGauge, Cog, LayoutDashboard } from "lucide-react";
+
+export const menuItems = [
+  { title: "Dashboard", icon: CircleGauge },
+  { title: "Extensions", icon: LayoutDashboard },
+  { title: "Settings", icon: Cog },
+  { title: "Help", icon: BadgeInfo },
+];
