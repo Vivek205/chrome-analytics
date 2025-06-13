637d0e4c83d48e3b92af0d43fc8c67fb8296b103
Vivek205 <ichbinvivek@gmail.com>
2025-06-13 08:56:57 +0200

    removed opennext and using vercel

--- open-next.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
- 
-export default defineCloudflareConfig({
-//   incrementalCache: r2IncrementalCache,
-});
-
-// TODO: Learn what is r2IncrementalCache
