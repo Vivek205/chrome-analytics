718226665a2509a98e1257daf2ef3f158dfd1510
Vivek205 <ichbinvivek@gmail.com>
2025-06-13 07:53:52 +0200

    fix: update database connection string to use NEXT_PUBLIC_DATABASE_URL

--- auth.ts
+++ auth.ts
@@ -7,7 +7,7 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth(() => {
   // Create a `Pool` inside the request handler.
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const pool = new Pool({ connectionString: process.env.NEXT_PUBLIC_DATABASE_URL });
   return {
     adapter: NeonAdapter(pool),
     providers: [Google({ allowDangerousEmailAccountLinking: true })],
