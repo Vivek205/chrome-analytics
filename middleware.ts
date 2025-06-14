1e4219087a86c5a73486c2d33befb6bf26c9cc18
Vivek205 <ichbinvivek@gmail.com>
2025-06-14 22:20:06 +0200

    refactor: Comment out unused session handling logic in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,21 @@ import { NextRequest, NextResponse } from "next/server";
 export const middleware = async (req: NextRequest) => {
   try {
     const session = await auth();
-    const { pathname, origin } = req.nextUrl;
+    // const { pathname, origin } = req.nextUrl;
 
-    if (!session && !pathname.startsWith("/login")) {
-      // TODO: Add RedirectBackTo functionality to redirect back to the original page after login
-      const url = req.nextUrl.clone();
-      url.pathname = "/login";
-      return NextResponse.redirect(url);
-    }
+    // if (!session && !pathname.startsWith("/login")) {
+    //   // TODO: Add RedirectBackTo functionality to redirect back to the original page after login
+    //   const url = req.nextUrl.clone();
+    //   url.pathname = "/login";
+    //   return NextResponse.redirect(url);
+    // }
 
-    if (session && pathname.startsWith("/login")) {
-      const homeUrl = req.nextUrl.clone();
-      homeUrl.pathname = "/";
-      homeUrl.search = ""; // Clear any search params
-      return NextResponse.redirect(homeUrl);
-    }
+    // if (session && pathname.startsWith("/login")) {
+    //   const homeUrl = req.nextUrl.clone();
+    //   homeUrl.pathname = "/";
+    //   homeUrl.search = ""; // Clear any search params
+    //   return NextResponse.redirect(homeUrl);
+    // }
 
     return NextResponse.next();
   } catch (error: any) {
