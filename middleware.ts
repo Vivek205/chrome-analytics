393981be73e13417a2767ea06f21fc6fd6bfaa0f
Vivek205 <ichbinvivek@gmail.com>
2025-06-14 22:52:33 +0200

    refactor: Simplify middleware logic and improve session handling

--- middleware.ts
+++ middleware.ts
@@ -1,50 +1,32 @@
-// import { auth } from "@/auth";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export const middleware = async (req: NextRequest) => {
-//   try {
-//     const session = await auth();
-//     const { pathname, origin } = req.nextUrl;
-
-//     if (!session && !pathname.startsWith("/login")) {
-//       // TODO: Add RedirectBackTo functionality to redirect back to the original page after login
-//       const url = req.nextUrl.clone();
-//       url.pathname = "/login";
-//       return NextResponse.redirect(url);
-//     }
-
-//     if (session && pathname.startsWith("/login")) {
-//       const homeUrl = req.nextUrl.clone();
-//       homeUrl.pathname = "/";
-//       homeUrl.search = ""; // Clear any search params
-//       return NextResponse.redirect(homeUrl);
-//     }
-
-//     return NextResponse.next();
-//   } catch (error: any) {
-//     console.error("Middleware error:", error);
-//     return new NextResponse(`Middleware error: ${error.message}`, {
-//       status: 500,
-//       headers: { "Content-Type": "text/plain" },
-//     });
-//   }
-// };
-
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
-
 import { NextResponse } from "next/server";
 import { auth } from "./auth";
 export default auth((req) => {
-  // req.auth
-  return new NextResponse(
-    `Authenticated request:- ${JSON.stringify(req.auth)}`,
-    {
-      status: 200,
-      headers: { "Content-Type": "text/plain" },
+  try {
+    const session = req.auth;
+    const { pathname } = req.nextUrl;
+    const currentUrl = req.nextUrl.clone();
+
+    if (!session && !pathname.startsWith("/login")) {
+      // TODO: Add RedirectBackTo functionality to redirect back to the original page after login
+
+      currentUrl.pathname = "/login";
+      return NextResponse.redirect(currentUrl);
     }
-  );
+
+    if (session && pathname.startsWith("/login")) {
+      currentUrl.pathname = "/";
+      currentUrl.search = ""; // Clear any search params
+      return NextResponse.redirect(currentUrl);
+    }
+
+    return NextResponse.next();
+  } catch (error: any) {
+    console.error("Middleware error:", error);
+    return new NextResponse(`Middleware error: ${error.message}`, {
+      status: 500,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
 });
 
 export const config = {
