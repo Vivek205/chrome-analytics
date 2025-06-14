f9aeef964cb006a6f9a3117c842e97ac070edfa6
Vivek205 <ichbinvivek@gmail.com>
2025-06-14 14:03:09 +0200

    refactor: Improve logging and streamline URL handling in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,21 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
   const session = await auth();
-  console.log("Session available:", session != null);
-  const { pathname } = req.nextUrl;
-  console.log("Current pathname:", pathname);
+  const { pathname, origin } = req.nextUrl;
+
   if (!session && !pathname.startsWith("/login")) {
-    console.log("Redirecting to login page because session is not available and pathname does not start with /login");
-    const loginUrl = new URL("/login", req.url);
-    console.log("Login URL:", loginUrl);
+    const loginUrl = new URL("/login", origin);
     // TODO: Add RedirectBackTo functionality to redirect back to the original page after login
     return NextResponse.redirect(loginUrl);
   }
 
   if (session && pathname.startsWith("/login")) {
-    console.log("Redirecting to request Url because session is available and pathname starts with /login");
-    const requestUrl = new URL("/", req.url);
-    console.log("Request URL:", requestUrl);
+    const requestUrl = new URL("/", origin);
     return NextResponse.redirect(requestUrl);
   }
 };
